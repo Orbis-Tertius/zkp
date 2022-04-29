05b5a21fc4ab224ddb69d6e6675ad736dc189c20
Faez Shakil <faez.shakil@gmail.com>
2022-04-29 09:42:40 +0500

    init

---
+++ README.md
@@ -0,0 +1,6 @@
+Denotational Zero Knowledge Proofs
+
+
+To enter development shell (if your nix has flakes enabled)
+
+nix develop .
---
+++ flake.nix
@@ -0,0 +1,31 @@
+{
+  description = "Denotational zkSnarks";
+
+  inputs = {
+    flake-utils.url = "github:numtide/flake-utils";
+    flake-compat = {
+      url = "github:edolstra/flake-compat";
+      flake = false;
+    };
+  };
+
+  outputs = { self, nixpkgs, flake-utils, ... }:
+  flake-utils.lib.eachDefaultSystem
+  (system:
+  let
+    pkgs = import nixpkgs { inherit system; };
+    agdaPkgs = pkgs;
+  in
+  {
+    devShell = pkgs.mkShell {
+      buildInputs = [
+        pkgs.nixpkgs-fmt
+        (agdaPkgs.agda.withPackages (ps: [
+          ps.standard-library
+        ]))
+      ];
+    };
+  }
+  );
+
+}
