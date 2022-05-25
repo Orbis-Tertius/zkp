a040af9650bb9dfb43ec4ad1463eff41da00d463
Faez Shakil <faez.shakil@gmail.com>
2022-05-25 16:16:21 +0500

    bump agda and std-lib

--- flake.nix
+++ flake.nix
@@ -7,21 +7,29 @@
       url = "github:edolstra/flake-compat";
       flake = false;
     };
+    agda.url = "github:agda/agda";
   };
 
-  outputs = { self, nixpkgs, flake-utils, ... }:
+  outputs = { self, nixpkgs, flake-utils, agda, ... }:
   flake-utils.lib.eachDefaultSystem
   (system:
   let
     pkgs = import nixpkgs { inherit system; };
-    agdaPkgs = pkgs;
   in
   {
     devShell = pkgs.mkShell {
       buildInputs = [
         pkgs.nixpkgs-fmt
-        (agdaPkgs.agda.withPackages (ps: [
-          ps.standard-library
+        (pkgs.agda.withPackages (ps: [
+          (ps.standard-library.overrideAttrs (oldAttrs: {
+            version = "2.0";
+            src =  pkgs.fetchFromGitHub {
+              repo = "agda-stdlib";
+              owner = "agda";
+              rev = "6e79234dcd47b7ca1d232b16c9270c33ff42fb84";
+              sha256 = "0n1xksqz0d2vxd4l45mxkab2j9hz9g291zgkjl76h5cn0p9wylk3";
+            };
+          }))
           ps.agda-categories
         ]))
       ];
